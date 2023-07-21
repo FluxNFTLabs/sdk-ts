7309741febdcb26443913a4392d46a805a325b01
nhannamsiu <dangduynam95@gmail.com>
2023-07-21 14:56:55 +0700

    feat: add get fnft example

---
+++ examples/indexer/4_GetNFTs/main.ts
@@ -0,0 +1,25 @@
+import * as fnfttypes from '../../../chain/flux/indexer/fnft/query';
+import { NodeHttpTransport } from '@improbable-eng/grpc-web-node-http-transport';
+
+const main = async () => {
+  const host = 'http://localhost:4445';
+  const cc = new fnfttypes.GrpcWebImpl(host, {
+    transport: NodeHttpTransport(),
+  })
+  const client = new fnfttypes.FNFTClientImpl(cc)
+
+  const req: fnfttypes.GetNFTsRequest = {
+    classId: "series",
+    id: "",
+    owner: "",
+  };
+
+  try {
+    const res = await client.GetNFTs(req)
+    console.log(res)
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+main()

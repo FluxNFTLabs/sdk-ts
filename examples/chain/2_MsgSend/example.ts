609c1903357e26267c410abc93555ee7965bcf6e
nhannamsiu <dangduynam95@gmail.com>
2023-08-01 15:18:36 +0700

    feat: building basic msg send logic

--- examples/chain/2_MsgSend/example.ts
+++ examples/chain/2_MsgSend/example.ts
@@ -1,37 +1,45 @@
-import cosmosclient from '@cosmos-client/core';
+import * as ethwallet from '@ethereumjs/wallet'
+import * as ethutil from '@ethereumjs/util'
+import { keccak256 } from 'ethereum-cryptography/keccak.js'
+import * as bech32 from 'bech32'
+
 import * as banktypes from '../../../chain/cosmos/bank/v1beta1/tx'
 import * as txtypes from '../../../chain/cosmos/tx/v1beta1/tx'
+import * as secp256k1 from '../../../chain/flux/crypto/v1beta1/ethsecp256k1/keys'
+import * as signingtypes from '../../../chain/cosmos/tx/signing/v1beta1/signing'
 import * as signingtypes from '../../../chain/cosmos/tx/signing/v1beta1/signing'
 import * as anytypes from '../../../chain/google/protobuf/any'
-import {MsgSendDesc} from "../../../chain/cosmos/bank/v1beta1/tx";
-import {Coin} from "../../../chain/cosmos/base/v1beta1/coin";
+
+function hexToBech32(hexBytes: ArrayLike<number>, prefix: string): string {
+  const words = bech32.bech32.toWords(hexBytes);
+  return bech32.bech32.encode(prefix, words);
+}
 
 const main = async () => {
-  // update sdk prefix
-  const bech32Prefix = {
-    accAddr: 'lux',
-    accPub: 'luxpub',
-    valAddr: 'luxvaloper',
-    valPub: 'luxvaloperpub',
-    consAddr: 'luxvalcons',
-    consPub: 'luxvalconspub',
-  };
-  cosmosclient.config.setBech32Prefix(bech32Prefix)
+  // init account
+  const wallet = ethwallet.Wallet.fromPrivateKey(Uint8Array.from(Buffer.from("88CBEAD91AEE890D27BF06E003ADE3D4E952427E88F88D31D61D3EF5E5D54305", 'hex')))
+  const senderPrivKey: secp256k1.PrivKey = {key: wallet.getPrivateKey()}
+  const senderPubkey: secp256k1.PubKey = {key: wallet.getPublicKey()}
+  const senderPubkeyAny: anytypes.Any = {
+    typeUrl: secp256k1.PubKey.$type,
+    value: secp256k1.PubKey.encode(senderPubkey).finish()
+  }
+  const senderAddr = hexToBech32(wallet.getAddress(), 'lux')
+  const receiverAddr = 'lux1jcltmuhplrdcwp7stlr4hlhlhgd4htqhu86cqx'
 
   // init msg
   const msg: banktypes.MsgSend = {
-    fromAddress: 'lux1cml96vmptgw99syqrrz8az79xer2pcgp209sv4',
-    toAddress: 'lux1cml96vmptgw99syqrrz8az79xer2pcgp209sv4',
+    fromAddress: senderAddr,
+    toAddress: receiverAddr,
     amount: [{ denom: 'lux', amount: '1' }],
   }
 
   const msgAny: anytypes.Any = {
-    typeUrl: banktypes.MsgSendDesc.service + banktypes.MsgSendDesc.methodName,
-    value: banktypes.MsgSend.encode(msg).finish()
+    typeUrl: banktypes.MsgSend.$type,
+    value: banktypes.MsgSend.encode(msg).finish(),
   }
 
-  banktypes.MsgSend[Symbol.hasInstance]
-
+  // prep tx data
   const txBody: txtypes.TxBody = {
     messages: [msgAny],
     memo: 'abc',
@@ -43,8 +51,8 @@ const main = async () => {
   const authInfo: txtypes.AuthInfo = {
     signerInfos: [
       {
-        public_key: anytypes.Any.,
-        mode_info: {
+        publicKey: senderPubkeyAny,
+        modeInfo: {
           single: {
             mode: signingtypes.SignMode.SIGN_MODE_DIRECT,
           },
@@ -57,14 +65,55 @@ const main = async () => {
         {denom: "lux", amount: "500000"}
       ],
       gasLimit: "500000",
-      payer: null,
-      granter: null
+      payer: "",
+      granter: ""
     },
+    tip: undefined,
   }
 
-  console.log(banktypes.MsgSendDesc)
+  const signDoc: txtypes.SignDoc = {
+    bodyBytes: txtypes.TxBody.encode(txBody).finish(),
+    authInfoBytes: txtypes.AuthInfo.encode(authInfo).finish(),
+    chainId: 'flux-1',
+    accountNumber: '4',
+  }
+  const signBytes = txtypes.SignDoc.encode(signDoc).finish()
+
+  // build tx
+  const msgHash = Buffer.from(keccak256(signBytes))
+  const sig = ethutil.ecsign(msgHash, Buffer.from(senderPrivKey.key))
 
 
+  // func SignatureDataToProto(data SignatureData) *SignatureDescriptor_Data {
+  //   switch data := data.(type) {
+  //   case *SingleSignatureData:
+  //         return &SignatureDescriptor_Data{
+  //       Sum: &SignatureDescriptor_Data_Single_{
+  //         Single: &SignatureDescriptor_Data_Single{
+  //           Mode:      data.SignMode,
+  //               Signature: data.Signature,
+  //         },
+  //       },
+  //     }
+  //   case *MultiSignatureData:
+  //         descDatas := make([]*SignatureDescriptor_Data, len(data.Signatures))
+  //
+  //     for j, d := range data.Signatures {
+  //       descDatas[j] = SignatureDataToProto(d)
+  //     }
+  //
+  //     return &SignatureDescriptor_Data{
+  //       Sum: &SignatureDescriptor_Data_Multi_{
+  //         Multi: &SignatureDescriptor_Data_Multi{
+  //           Bitarray:   data.BitArray,
+  //               Signatures: descDatas,
+  //         },
+  //       },
+  //     }
+  //   default:
+  //     panic(fmt.Errorf("unexpected case %+v", data))
+  //   }
+  // }
 }
 
 main()

69799c2d4443bf2dbb6e619451b0b35640870e3a
nhannamsiu <dangduynam95@gmail.com>
2023-08-03 16:12:45 +0700

    feat: finish msg send delegation proto

--- examples/chain/3_MsgSendDelegationProto/example.ts
+++ examples/chain/3_MsgSendDelegationProto/example.ts
@@ -4,13 +4,14 @@ import { keccak256 } from 'ethereum-cryptography/keccak.js'
 import * as bech32 from 'bech32'
 import { NodeHttpTransport } from '@improbable-eng/grpc-web-node-http-transport';
 
+import * as anytypes from '../../../chain/google/protobuf/any'
 import * as banktypes from '../../../chain/cosmos/bank/v1beta1/tx'
 import * as txtypes from '../../../chain/cosmos/tx/v1beta1/tx'
 import * as txservice from '../../../chain/cosmos/tx/v1beta1/service'
 import * as authservice from '../../../chain/cosmos/auth/v1beta1/query'
 import * as secp256k1 from '../../../chain/flux/crypto/v1beta1/ethsecp256k1/keys'
 import * as signingtypes from '../../../chain/cosmos/tx/signing/v1beta1/signing'
-import * as anytypes from '../../../chain/google/protobuf/any'
+import * as web3gwtypes from '../../../chain/flux/indexer/web3gw/query'
 
 function hexToBech32(hexBytes: ArrayLike<number>, prefix: string): string {
   const words = bech32.bech32.toWords(hexBytes);
@@ -25,15 +26,19 @@ function compressPublicKey(uncompressedPublicKey: Buffer): Buffer {
 }
 
 const main = async () => {
-  // init client
-  const host = 'http://localhost:9091';
-  const cc = new txservice.GrpcWebImpl(host, {
+  // init clients
+  const cc = new txservice.GrpcWebImpl('http://localhost:9091', {
     transport: NodeHttpTransport(),
   })
   const txClient = new txservice.ServiceClientImpl(cc)
   const authClient = new authservice.QueryClientImpl(cc)
 
-  // init account
+  const web3gwCC = new web3gwtypes.GrpcWebImpl('http://localhost:4445', {
+    transport: NodeHttpTransport(),
+  })
+  const web3gwClient = new web3gwtypes.Web3GWClientImpl(web3gwCC)
+
+  // init accounts
   const wallet = ethwallet.Wallet.fromPrivateKey(Uint8Array.from(Buffer.from("88CBEAD91AEE890D27BF06E003ADE3D4E952427E88F88D31D61D3EF5E5D54305", 'hex')))
   const senderPrivKey: secp256k1.PrivKey = {key: wallet.getPrivateKey()}
   const senderPubkey: secp256k1.PubKey = {key: compressPublicKey(Buffer.from(wallet.getPublicKey()))}
@@ -46,8 +51,20 @@ const main = async () => {
 
   // fetch account num, seq
   const senderInfo = await authClient.AccountInfo({address: senderAddr})
-  const accNum = senderInfo.info!.accountNumber!
-  const accSeq = senderInfo.info!.sequence!
+  const senderAccNum = senderInfo.info!.accountNumber!
+  const senderAccSeq = senderInfo.info!.sequence!
+
+  // fetch web3gw metadata
+  const metadata = await web3gwClient.GetMetaData({})
+  const feePayerPubKey: secp256k1.PubKey = {key: metadata.pubkey}
+  const feePayerPubKeyAny: anytypes.Any = {
+    typeUrl: '/' + secp256k1.PubKey.$type,
+    value: secp256k1.PubKey.encode(feePayerPubKey).finish()
+  }
+  const feePayerAddr = metadata.address
+  const feePayerInfo = await authClient.AccountInfo({address: feePayerAddr})
+  const feePayerAccNum = feePayerInfo.info!.accountNumber!
+  const feePayerAccSeq = feePayerInfo.info!.sequence!
 
   // init msg
   const msg: banktypes.MsgSend = {
@@ -55,7 +72,6 @@ const main = async () => {
     toAddress: receiverAddr,
     amount: [{ denom: 'lux', amount: '1' }],
   }
-
   const msgAny: anytypes.Any = {
     typeUrl: '/' + banktypes.MsgSend.$type,
     value: banktypes.MsgSend.encode(msg).finish(),
@@ -79,7 +95,16 @@ const main = async () => {
             mode: signingtypes.SignMode.SIGN_MODE_DIRECT,
           },
         },
-        sequence: accSeq,
+        sequence: senderAccSeq,
+      },
+      {
+        publicKey: feePayerPubKeyAny,
+        modeInfo: {
+          single: {
+            mode: signingtypes.SignMode.SIGN_MODE_DIRECT,
+          },
+        },
+        sequence: feePayerAccSeq,
       },
     ],
     fee: {
@@ -87,38 +112,45 @@ const main = async () => {
         {denom: "lux", amount: "100000000000000"}
       ],
       gasLimit: "200000",
-      payer: "",
+      payer: feePayerAddr,
       granter: ""
     },
     tip: undefined,
   }
 
-  const signDoc: txtypes.SignDoc = {
+  // get signatures
+  let signDoc: txtypes.SignDoc = {
     bodyBytes: txtypes.TxBody.encode(txBody).finish(),
     authInfoBytes: txtypes.AuthInfo.encode(authInfo).finish(),
     chainId: 'flux-1',
-    accountNumber: accNum,
+    accountNumber: senderAccNum,
   }
-  const signBytes = txtypes.SignDoc.encode(signDoc).finish()
-
-  // build tx
+  let signBytes = txtypes.SignDoc.encode(signDoc).finish()
   const msgHash = Buffer.from(keccak256(signBytes))
-  const sig = ethutil.ecsign(msgHash, Buffer.from(senderPrivKey.key))
-  const cosmosSig = Uint8Array.from(Buffer.concat([sig.r, sig.s, Buffer.from([0])]))
 
-  console.log(sig.v.toString())
+  const senderSign = ethutil.ecsign(msgHash, Buffer.from(senderPrivKey.key))
+  const senderCosmosSig = Uint8Array.from(Buffer.concat([senderSign.r, senderSign.s, Buffer.from([0])]))
+
+  signDoc = {
+    bodyBytes: txtypes.TxBody.encode(txBody).finish(),
+    authInfoBytes: txtypes.AuthInfo.encode(authInfo).finish(),
+    chainId: 'flux-1',
+    accountNumber: feePayerAccNum,
+  }
+  signBytes = txtypes.SignDoc.encode(signDoc).finish()
+  const res = await web3gwClient.SignProto({data: signBytes})
+  const feePayerCosmosSig = res.signature
 
   // broadcast tx
   const txRaw: txtypes.TxRaw = {
     bodyBytes: txtypes.TxBody.encode(txBody).finish(),
     authInfoBytes: txtypes.AuthInfo.encode(authInfo).finish(),
-    signatures: [cosmosSig],
+    signatures: [senderCosmosSig, feePayerCosmosSig],
   }
   const broadcastReq: txservice.BroadcastTxRequest = {
     txBytes: txtypes.TxRaw.encode(txRaw).finish(),
     mode: txservice.BroadcastMode.BROADCAST_MODE_SYNC,
   }
-
   try {
     const res = await txClient.BroadcastTx(broadcastReq)
     console.log(res)

4c56b5086b4fc2abf062897d55fbcb1ed91655be
nhannamsiu <dangduynam95@gmail.com>
2023-08-06 23:07:46 +0700

    feat: align diff with cosmos-sdk eip712

--- a.ts
+++ a.ts
@@ -1,73 +1,69 @@
-const rootTypes = {
-  "EIP712Domain": [
+interface Eip712Types {
+  [key: string]: any
+}
+let rootTypes: Eip712Types = {
+  'EIP712Domain': [
     {
-      Name: "name",
-          Type: "string",
+      name: 'name',
+      type: 'string',
     },
     {
-      Name: "version",
-          Type: "string",
+      name: 'version',
+      type: 'string',
     },
     {
-      Name: "chainId",
-          Type: "uint256",
+      name: 'chainId',
+      type: 'uint256',
     },
     {
-      Name: "verifyingContract",
-          Type: "string",
+      name: 'verifyingContract',
+      type: 'string',
     },
     {
-      Name: "salt",
-          Type: "string",
+      name: 'salt',
+      type: 'string',
     },
   ],
-  "Tx": [
-    {Name: "account_number", Type: "string"},
-    {Name: "chain_id", Type: "string"},
-    {Name: "fee", Type: "Fee"},
-    {Name: "memo", Type: "string"},
-    {Name: "msgs", Type: "Msg[]"},
-    {Name: "sequence", Type: "string"},
-    {Name: "timeout_height", Type: "string"},
+  'Tx': [
+    {name: 'account_number', type: 'string'},
+    {name: 'chain_id', type: 'string'},
+    {name: 'fee', type: 'Fee'},
+    {name: 'memo', type: 'string'},
+    {name: 'msgs', type: 'Msg[]'},
+    {name: 'sequence', type: 'string'},
+    {name: 'timeout_height', type: 'string'},
   ],
-  "Fee": [
-    {Name: "amount", Type: "Coin[]"},
-    {Name: "gas", Type: "string"},
+  'Fee': [
+    {name: 'feePayer', type: 'string'},
+    {name: 'amount', type: 'Coin[]'},
+    {name: 'gas', type: 'string'},
   ],
-  "Coin": [
-    {Name: "denom", Type: "string"},
-    {Name: "amount", Type: "string"},
+  'Coin': [
+    {name: 'denom', type: 'string'},
+    {name: 'amount', type: 'string'},
   ],
-  "Msg": [
-    {Name: "type", Type: "string"},
-    {Name: "value", Type: "MsgValue"},
+  'Msg': [
+    {name: 'type', type: 'string'},
+    {name: 'value', type: 'MsgValue'},
   ],
+  PlaceHolder: [],
 }
 
-const msg= {
-  msgs: [
+const msgs = [
     {
       "type": "cosmos-sdk/MsgSend",
       "value": {
         "amount": [
-          {
-            "amount": "77",
-            "denom": "lux"
-          }
+            {
+              "amount": "77",
+              "denom": "lux"
+            },
         ],
-        "from_address": "lux1cml96vmptgw99syqrrz8az79xer2pcgp209sv4",
-        "to_address": "lux1jcltmuhplrdcwp7stlr4hlhlhgd4htqhu86cqx"
-      }
-    }
-  ]
-}
-
-interface Eip712Types {
-  [key: string]: any
-}
-let eip712Types: Eip712Types = {
-  tx: [],
-}
+          "from_address": "lux1cml96vmptgw99syqrrz8az79xer2pcgp209sv4",
+          "to_address": "lux1jcltmuhplrdcwp7stlr4hlhlhgd4htqhu86cqx",
+      },
+    },
+]
 
 function walkNestedJSON(jsonObj: any, parentKey: string = ''): void {
   // iterate json object
@@ -76,8 +72,8 @@ function walkNestedJSON(jsonObj: any, parentKey: string = ''): void {
     // handle array field
     if (Array.isArray(value)) {
       const childKey = 'Type' + key.charAt(0).toUpperCase() + key.slice(1)
-      eip712Types[childKey] = []
-      eip712Types[parentKey].push({
+      rootTypes[childKey] = []
+      rootTypes[parentKey].push({
         name: key,
         type: childKey + '[]',
       })
@@ -88,8 +84,8 @@ function walkNestedJSON(jsonObj: any, parentKey: string = ''): void {
     // handle object field
     if (!Array.isArray(value) && typeof value === 'object') {
       const childKey = 'Type' + key.charAt(0).toUpperCase() + key.slice(1)
-      eip712Types[childKey] = []
-      eip712Types[parentKey].push({
+      rootTypes[childKey] = []
+      rootTypes[parentKey].push({
         name: key,
         type: childKey,
       })
@@ -97,15 +93,41 @@ function walkNestedJSON(jsonObj: any, parentKey: string = ''): void {
       continue
     }
 
-
     // handle primary field
-    console.log('primary', key, value)
-    eip712Types[parentKey].push({
+    rootTypes[parentKey].push({
       name: key,
       type: typeof value,
     })
   }
 }
 
-walkNestedJSON(msg, 'tx')
-console.log(eip712Types)
+
+function deepSortObject(obj: any): any {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj; // Base case: return non-object values as-is
+  }
+
+  if (Array.isArray(obj)) {
+    // If it's an array, sort each element recursively
+    return obj.map(deepSortObject);
+  }
+
+  // For objects, sort the keys and create a new object with sorted keys and sorted values
+  const sortedObj: any = {};
+  Object.keys(obj).sort().forEach((key) => {
+    sortedObj[key] = deepSortObject(obj[key]);
+  });
+
+  return sortedObj;
+}
+
+walkNestedJSON(msgs, 'PlaceHolder')
+
+// post-processing
+delete rootTypes['PlaceHolder']
+delete rootTypes['Type0']
+rootTypes['MsgValue'] = rootTypes['TypeValue']
+delete rootTypes['TypeValue']
+rootTypes = deepSortObject(rootTypes)
+
+console.log(rootTypes)

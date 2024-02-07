b6aa83a22851ab1ef63e064d9e92afaba26d2926
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-02-07 10:10:59 +0700

    feat: implement text field component

---
+++ packages/ui/components/TextField/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  errorMessage: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+<template>
+  <div class="base-text-field" :class="errorMessage ? 'invalid' : ''">
+    <p class="label">
+      {{ label }}
+    </p>
+    <input class="input" type="text" v-bind="$attrs" />
+    <p class="message">{{ errorMessage }}</p>
+  </div>
+</template>
--- packages/ui/main.js
+++ packages/ui/main.js
@@ -1,7 +1,8 @@
 // plugins/global-components.js
 import { defineNuxtPlugin } from '#app'
-import BaseButton from './components/button/index.vue'
-
+import BaseButton from './components/Button/index.vue'
+import BaseTextField from './components/TextField/index.vue'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('BaseButton', BaseButton)
+  nuxtApp.vueApp.component('BaseTextField', BaseTextField)
 })

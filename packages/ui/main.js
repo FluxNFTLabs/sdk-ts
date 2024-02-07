6a9f01c7b1d17bc64545ba256496332e7adb99df
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-02-07 10:58:05 +0700

    feat: implement tooltip

--- packages/ui/main.js
+++ packages/ui/main.js
@@ -2,7 +2,9 @@
 import { defineNuxtPlugin } from '#app'
 import BaseButton from './components/Button/index.vue'
 import BaseTextField from './components/TextField/index.vue'
+import BaseTooltip from './components/Tooltip/index.vue'
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('BaseButton', BaseButton)
   nuxtApp.vueApp.component('BaseTextField', BaseTextField)
+  nuxtApp.vueApp.component('BaseTooltip', BaseTooltip)
 })

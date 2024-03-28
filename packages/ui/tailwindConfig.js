e25ea5ddbb6ad173393ebaaeac54d19d489f6ba2
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-03-28 14:31:05 +0700

    update: icons

--- packages/ui/tailwindConfig.js
+++ packages/ui/tailwindConfig.js
@@ -81,7 +81,7 @@ export const blueGray = {
   50: '#F8FAFC',
   100: '#F1F5F9',
   200: '#E2E8F0', //border secondary
-  300: '#CBD5E1',
+  300: '#CBD5E1', // --Fg-Tertiary
   400: '#94A3B8',
   500: '#64748B',
   600: '#475569',

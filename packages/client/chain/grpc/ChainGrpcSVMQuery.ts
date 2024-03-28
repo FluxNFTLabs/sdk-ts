7e2ee42d16470be561b7bc6d8b50c149acf1b79d
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-03-28 17:51:14 +0700

    feat: add SVM query

---
+++ packages/client/chain/grpc/ChainGrpcSVMQuery.ts
@@ -0,0 +1,23 @@
+import BaseGrpc from '../../BaseGrpc'
+import * as svmQuery from '../../../../chain/flux/svm/v1beta1/query'
+import * as web3 from '@solana/web3.js'
+export class ChainGrpcSVMQuery extends BaseGrpc {
+  protected client: svmQuery.QueryClientImpl
+  constructor(endpoint: string) {
+    super(endpoint)
+    this.client = new svmQuery.QueryClientImpl(this.getGrpcWebImpl(endpoint))
+  }
+  async programAccounts({
+    address
+  }: {
+    address: string
+  }): Promise<svmQuery.ProgramAccountsResponse> {
+    const request = svmQuery.ProgramAccountsRequest.create({
+      address: ''
+    })
+    const response: svmQuery.ProgramAccountsResponse = await this.retry(() =>
+      this.client.ProgramAccounts(request)
+    )
+    return response
+  }
+}

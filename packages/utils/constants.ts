c78a520250340bea1a6b047e487651ee9cc2e222
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-04-09 09:46:26 +0700

    chore: update gas limit

--- packages/utils/constants.ts
+++ packages/utils/constants.ts
@@ -1,5 +1,4 @@
-import BigNumber from './classes/BigNumber/BigNumber'
-export const DEFAULT_GAS_LIMIT = 70000000
+export const DEFAULT_GAS_LIMIT = 30200000
 export const DEFAULT_GAS_PRICE = 100000000000000
 export const DEFAULT_BLOCK_TIMEOUT_HEIGHT = 30
 export const DEFAULT_STD_FEE = {

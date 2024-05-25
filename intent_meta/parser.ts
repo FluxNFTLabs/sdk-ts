dcc34fc296e7ccf5703f19ddab4115688f1dd86f
Phuc Ta <vinhphuctadang@gmail.com>
2024-05-25 16:20:47 +0700

    feat: wip: convert meta+input => trigger msg

--- intent_meta/parser.ts
+++ intent_meta/parser.ts
@@ -1,4 +1,6 @@
-import { Schema, SchemaPrompt, StrategyMetadata } from "../chain/flux/strategy/v1beta1/strategy";
+import { FISQueryInstruction, FISQueryRequest, queryActionFromJSON } from "../chain/flux/astromesh/v1beta1/query";
+import { Plane, planeFromJSON } from "../chain/flux/astromesh/v1beta1/tx";
+import { Schema, SchemaFISQuery, SchemaPrompt, StrategyMetadata } from "../chain/flux/strategy/v1beta1/strategy";
 import { MsgTriggerStrategies } from "../chain/flux/strategy/v1beta1/tx";
 
 function deepClone(a: any): any {
@@ -9,29 +11,64 @@ function replacePlaceholders(template, values) {
     return template.replace(/\${(\w+)}/g, (_, key: string) => values[key] || '');
 }
 
-function interprete(action: string, prompt: SchemaPrompt, input: Map<string, any>): MsgTriggerStrategies {
-  // fill input into query
-  // convert query to fis query
-  return MsgTriggerStrategies.create({})
-}
-
 /*
-Pending Todo: input address by plane?
-E.g query order of an address (calculated from programId and some seeds)
+  Pending Todo: input address by plane?
+  E.g query order of an address (calculated from programId and some seeds) => need to support equation eval() with input params
 */
+
+function compileTriggerMsg(
+  luxSender: string,
+  intentId: string,
+  action: string, 
+  prompt: SchemaPrompt, 
+  userInput: Object,
+  defaultConst: Object,
+): MsgTriggerStrategies {
+  // fill input into query + convert compiled query to fis query
+  let query = deepClone(prompt.query) as SchemaFISQuery
+  let knownVars = {...userInput, ...defaultConst}
+
+  let fisQuery = FISQueryRequest.create({
+    instructions: [],
+  })
+  for(let ix of query.instructions) {
+    // replace vars within queries
+    for(let i = 0; i < ix.input.length; i++) {
+      console.log(ix.input[i])
+      ix.input[i] = replacePlaceholders(Buffer.from(ix.input[i]).toString(), knownVars)
+    }
+
+    fisQuery.instructions.push(FISQueryInstruction.create({
+      plane: planeFromJSON(ix.plane),
+      action: queryActionFromJSON(ix.action),
+      address: ix.address,
+      input: ix.input,
+    }))
+  }
+
+  let strategyInput = {
+    [action]: {...userInput},
+  }
+  return MsgTriggerStrategies.create({
+    sender: luxSender,
+    ids: [intentId],
+    inputs: [Buffer.from(JSON.stringify(strategyInput))],
+    queries: [fisQuery],
+  })
+}
 
 let metadata = Schema.create({
   groups: [
     {
       name: "deposit/transfer helper",
       prompts: {
-        "deposit": {
+        deposit: {
           template: "deposit \${amount} \${denom} equally from bank to all planes",
           query: {
             instructions: [
               {
-                plane: "0",
-                action: "2",
+                plane: "COMSOS",
+                action: "COSMOS_ASTROMESH_TRANSFER",
                 "address": null,
                 "input": [
                   new Uint8Array(Buffer.from("${addressEvm}")),
@@ -44,87 +81,24 @@ let metadata = Schema.create({
       }
     }
   ]
-})
-
-// intent creator => define schema
-// metadata:
-//   intent image: url
-//   name: "intent name"
-//   description: "Defi user joins this"
-//   tags: [#defi, #nft]
-let metadataJson = `
-{
-    "groups": [
-      {
-        "name": "deposit/transfer helper",
-        "prompts": {
-          "deposit": {
-            "template": "deposit \${amount} \${denom} equally from bank to all planes",
-            "query": {
-              "instructions": [
-                {
-                  "plane": 0,
-                  "action": 2,
-                  "address": null,
-                  "input": [
-                    "\${wallet}",
-                    "\${denom}"
-                  ]
-                }
-              ]
-            }
-          },
-          "withdraw": {
-            "template": "withdraw \${denom} from all planes to cosmos bank"
-          }
-        }
-      },
-      {
-        "name": "DeFi helper",
-        "prompts": {
-          "provide_liquidity": {
-            "template": "provide \${amountA} \${denomA} and \${amountB} \${denomB} to liquidity pool. We cannot try this for now"
-          }
-        }
-      }
-    ]
-}
-`;
+});
 
 (async() => {
-    // fill order => global vars => input vars => default fields => queries
-    let metadata = JSON.parse(metadataJson)
-    let template = metadata.groups[0].prompts[0]
-    let filled_fields = {
-        amount: "100",
-        denom: 'lux',
-    }
-    let globalVars = { wallet: 'lux123456789', ...filled_fields}
-    let abstractionDefaultFields = deepClone(template.defaults)
-    for(let k in abstractionDefaultFields) {
-        abstractionDefaultFields[k] = replacePlaceholders(abstractionDefaultFields[k], globalVars)
-    }
+  let consts = {
+    wallet: 'lux1jcltmuhplrdcwp7stlr4hlhlhgd4htqhu86cqx',
+  }
+  let inputVars = {
+    denom: "usdt",
+    amount: '30000000',
+  }
+  let triggerMsg = compileTriggerMsg(
+    'lux1jcltmuhplrdcwp7stlr4hlhlhgd4htqhu86cqx',
+    '9eb83888b44a71f3a1630676aa1f3052deb142bb7661e64a71b7b77938088dd7',
+    'deposit_equally',
+    metadata.groups[0].prompts.deposit,
+    inputVars,
+    consts
+  )
 
-    let abstractionObject = {
-        ...abstractionDefaultFields,
-        ...filled_fields,
-    }
-
-    let actual_queries = template.query
-
-    let knownVars = {
-        ...abstractionObject,
-        ...globalVars,
-    }
-
-    let query = deepClone(actual_queries)
-    for(let ix of query.instructions) {
-        // TODO: Replace other fields
-        for (let i = 0; i < ix.input.length; i++) {
-            ix.input[i] = replacePlaceholders(ix.input[i], knownVars)
-        }
-    }
-
-    console.log("abstraction object:", JSON.stringify(abstractionObject, null, '  '))
-    console.log("query:", JSON.stringify(query, null, '  '))
+  console.log(triggerMsg)
 })()

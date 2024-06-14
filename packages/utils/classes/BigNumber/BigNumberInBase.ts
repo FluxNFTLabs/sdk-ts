638040b1082fa7d701da65311693ee97dd1a26ed
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2024-06-14 22:05:17 +0700

    feat: update bignumber class

--- packages/utils/classes/BigNumber/BigNumberInBase.ts
+++ packages/utils/classes/BigNumber/BigNumberInBase.ts
@@ -1,41 +1,42 @@
-import type BigNumberInWei from './BigNumberInWei'
-import BigNumber from './BigNumber'
-import { bigNumberBaseToWei } from './utils'
+import type BigNumberInWei from './BigNumberInWei';
+import BigNumber from './BigNumber';
+import { bigNumberBaseToWei } from './utils';
 
 export default class BigNumberInBase extends BigNumber {
+  // Other methods and properties
   static make(number: BigNumber.Value): BigNumberInBase {
-    return new BigNumberInBase(number)
+    return new BigNumberInBase(number);
   }
 
-  minus(n: BigNumber.Value, base?: number): BigNumberInBase {
-    return new BigNumberInBase(super.minus(n, base))
+  override minus(n: BigNumber.Value, base?: number): BigNumberInBase {
+    return new BigNumberInBase(super.minus(n, base));
   }
 
-  plus(n: BigNumber.Value, base?: number): BigNumberInBase {
-    return new BigNumberInBase(super.plus(n, base))
+  override plus(n: BigNumber.Value, base?: number): BigNumberInBase {
+    return new BigNumberInBase(super.plus(n, base));
   }
 
-  dividedBy(n: BigNumber.Value, base?: number): BigNumberInBase {
-    return new BigNumberInBase(super.dividedBy(n, base))
+  override dividedBy(n: BigNumber.Value, base?: number): BigNumberInBase {
+    return new BigNumberInBase(super.dividedBy(n, base));
   }
 
-  div(n: BigNumber.Value, base?: number): BigNumberInBase {
-    return new BigNumberInBase(super.div(n, base))
+  override div(n: BigNumber.Value, base?: number): BigNumberInBase {
+    return new BigNumberInBase(super.div(n, base));
   }
 
-  multipliedBy(n: BigNumber.Value, base?: number): BigNumberInBase {
-    return new BigNumberInBase(super.multipliedBy(n, base))
+  override multipliedBy(n: BigNumber.Value, base?: number): BigNumberInBase {
+    return new BigNumberInBase(super.multipliedBy(n, base));
   }
 
-  times(n: BigNumber.Value, base?: number): BigNumberInBase {
-    return new BigNumberInBase(super.times(n, base))
+  override times(n: BigNumber.Value, base?: number): BigNumberInBase {
+    return new BigNumberInBase(super.times(n, base));
   }
 
-  pow(n: BigNumber.Value, base?: number): BigNumberInBase {
-    return new BigNumberInBase(super.pow(n, base))
+  override pow(n: BigNumber.Value, base?: number): BigNumberInBase {
+    return new BigNumberInBase(super.pow(n, base));
   }
 
   toWei(decimals = 18): BigNumberInWei {
-    return bigNumberBaseToWei(this, decimals)
+    return bigNumberBaseToWei(this, decimals);
   }
 }

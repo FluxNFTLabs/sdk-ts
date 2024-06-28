14f497367852c46649a6b9e688e798f8b81d67a4
Phuc Ta <vinhphuctadang@gmail.com>
2024-06-28 21:11:37 +0700

    Revert "feat: trigger arbitrage example"
    This reverts commit fd9835bca3f609c40b7c6231695e0a8b1bc62bff.

--- packages/client/chain/grpc/ChainGrpcStrategyQuery.ts
+++ packages/client/chain/grpc/ChainGrpcStrategyQuery.ts
@@ -6,9 +6,9 @@ export class ChainGrpcStrategyQuery extends BaseGrpc {
     super(endpoint)
     this.client = new chainFluxStrategyV1beta1Query.QueryClientImpl(this.getGrpcWebImpl(endpoint))
   }
-  async listStrategy(request: any): Promise<chainFluxStrategyV1beta1Query.ListStrategiesResponse> {
-    const response: chainFluxStrategyV1beta1Query.ListStrategiesResponse = await this.retry(() =>
-      this.client.ListStrategies(request)
+  async listStrategy(request: any): Promise<chainFluxStrategyV1beta1Query.ListStrategyResponse> {
+    const response: chainFluxStrategyV1beta1Query.ListStrategyResponse = await this.retry(() =>
+      this.client.ListStrategy(request)
     )
     return response
   }
